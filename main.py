74ea5d5769a2d142d88ba8e38803961429051307
carlosantos2103 <carlosantos21.03@gmail.com>
2020-10-30 18:20:22 +0000

    First regular expressions defined

--- main.py
+++ main.py
@@ -1,1 +1,64 @@
 # main.py
+
+import ply.lex as lex
+from utils import readFile
+
+tokens = ("RESULT", "STAGE", "COMMENT", "ALLSTAGES")
+states = (
+    ("stage", "exclusive"),
+    ("comment", "exclusive"),
+)
+
+
+def t_RESULT(t):
+    r"""[a-z ]+"""
+    if "o" in t.value:
+        t.value = t.value[:-1]
+    t.lexer.begin("stage")
+    return t
+
+
+def t_ALLSTAGES(t):
+    r"""[0-9]+..[0-9]+"""
+    return t
+
+
+def t_stage_STAGE(t):
+    r"""[0-9]+"""
+    t.lexer.begin("comment")
+    return t
+
+
+def t_comment_DIVISION(t):
+    r"""[ -]+"""
+    return t
+
+
+def t_comment_COMMENT(t):  # Ignores Everything that isnt a RESULT (ok/notok)
+    r"""[a-zA-Z0-9 ]+\n"""
+    t.lexer.begin("INITIAL")
+    return t
+
+
+def t_error(t):
+    print("Unknown token: [%s]" % t.value)
+    pass
+
+
+def t_comment_error(t):
+    t_error(t)
+
+
+def t_stage_error(t):
+    t_error(t)
+
+
+t_ignore = "\n"
+t_comment_ignore = " - "
+t_stage_ignore = ""
+
+lexer = lex.lex()
+lexer.input(readFile("testes/teste7.t"))
+
+for token in iter(lexer.token, None):
+    print(token)
