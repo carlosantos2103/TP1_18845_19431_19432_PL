1371195808a312181883151d7bc472185e483451
carlosantos2103 <carlosantos21.03@gmail.com>
2020-11-03 16:33:35 +0000

    Big update - main.py

--- main.py
+++ main.py
@@ -1,62 +1,80 @@
-# main.py
-
+# Trabalho_Pratico v1.0
 import ply.lex as lex
 from utils import readFile
 
-tokens = ("RESULT", "STAGE", "COMMENT", "ALLSTAGES")
+# 1..4
+# ok 1 - First test to something
+# not ok 2 - Something else being tested
+# ok 3 - Third test on something else
+# ok 4 - Fourth test
+
+ficheiros = ["teste3.t", "NULL"]
+
+tokens = ("TOTAL_TESTE", "ESTADO_TESTE", "NUMERO", "COMENTARIO")
+
 states = (
-    ("stage", "exclusive"),
-    ("comment", "exclusive"),
+    ("numero", "exclusive"),
+    ("comentario", "exclusive"),
 )
 
 
-def t_RESULT(t):
-    r"""[a-z ]+"""  # TODO: por "\t*" antes por causa dos subtestes?
+def t_TOTAL_TESTE(t):  # 1..41
+    r"""[0-9 ]+..[0-9]+"""
+    return t
+
+
+def t_ESTADO_TESTE(t):  # ok/not ok
+    r"""[o|k|n| |t]+"""
     if "o" in t.value:
         t.value = t.value[:-1]
-    t.lexer.begin("stage")
-    return t
+        t.lexer.begin("numero")
+        return t
+    pass
 
 
-def t_ALLSTAGES(t):
-    r"""[0-9]+..[0-9]+"""
-    return t
-
-
-def t_stage_STAGE(t):
+def t_numero_NUMERO(t):  # 1/2/3
     r"""[0-9]+"""
-    t.lexer.begin("comment")
+    t.value = int(t.value)
+    t.lexer.begin("comentario")
     return t
 
 
-def t_comment_COMMENT(t):  # TODO: Nao tenho a certeza se o comentario pode continuar na linha de baixo
-    r"""[a-zA-Z0-9 ]+\n([ ]+[^#][a-zA-Z0-9 ]+\n)*|\n"""
+def t_comentario_COMENTARIO(t):
+    # r"""[^\n]*\n""" \n --- \n   # asdasdas
+    r"""\n[ ]*[# ][a-zA-Z0-9: ]+\n|[a-zA-Z0-9: ]*\n"""
     t.lexer.begin("INITIAL")
     return t
 
 
 def t_error(t):
-    print("Unknown token: [%s]" % t.value)
-    pass
+    print("ERROR: \n" + t.value)
+    exit(1)
 
 
-def t_comment_error(t):
-    t_error(t)
+def t_comentario_error(t):
+    print("ERROR comentario: \n" + t.value)
+    exit(1)
 
 
-def t_stage_error(t):
-    t_error(t)
+def t_numero_error(t):
+    print("ERROR numero: \n" + t.value)
+    exit(1)
 
 
 t_ignore = "\n"
-t_comment_ignore = " - "
-t_stage_ignore = ""
+t_numero_ignore = ""
+t_comentario_ignore = " - "
 
 lexer = lex.lex()
-lexer.input(readFile("testes/teste2.t"))
 
-for token in iter(lexer.token, None):
-    if token.type == "RESULT":
-        print(token.value, end=" ")
-    if token.type == "STAGE":
-        print(token.value)
+lista = []
+
+nome_ficheiro = ""
+i = 0
+while ficheiros[i] != "NULL":
+    nome_ficheiro = ficheiros[i]
+    lexer.input(readFile(nome_ficheiro))
+    print(f"\n############ Ficheiro Nº{i + 1} ############\n")
+    for token in iter(lexer.token, None):
+        print(token)
+    i += 1
