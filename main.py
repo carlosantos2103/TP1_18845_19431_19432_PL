df673a98d132fc13dd793e216b871ccf466da836
Joao Ricardo Pinto Azevedo <jpinto.azevedo@gmail.com>
2020-11-07 12:43:34 +0000

    um resumo de numero de testes executados, numero de testes com resultado positivo e percentagem de falhas;

--- main.py
+++ main.py
@@ -107,17 +107,71 @@ for token in iter(lexer.token, None):
     #                   token.value) + " </span>")
     if token.type == "ESTADO_TESTE":
         level = token.value.count('    ') + 1
-        result = token.value[-2:]
+        if level == 1:
+            result = token.value
+            print(result)
+        else:
+            result = token.value[(level-1)*4:]
+            print(result)
     if token.type == "NUMERO":
         stage = token.value
     if token.type == "DESCRICAO":
         teste = Test(result, stage, token.value, level)
         lista.append(teste)
 
-    print(token)
+    #print(token)
+
+
+# Trabalhar o numero de testes executados, numero de testes com
+# resultado positivo e percentagem de falhas;
+contador_testes_negativos = 0
+contador_testes_positivos = 0
 
 for teste in lista:
-    print(f"{teste.result}\t{teste.stage}\t{teste.description}\t{teste.level}")
+    if teste.result == "ok":
+        contador_testes_positivos+=1
+    else:
+        contador_testes_negativos+=1
+
+
+print(f"\n\n  RELATORIO DO FICHEIRO: {nome_ficheiro[7:]}")
+print(f". O Numero Total de Testes Executados: {contador_testes_negativos+contador_testes_positivos}")
+print(f". O Numero Total de Testes Positivos: {contador_testes_positivos}")
+print(f". O Numero Total de Testes Negativos: {contador_testes_negativos}")
+print(f". A Percentagem Total de Falhas: %0.2f %% \n" % (contador_testes_negativos/(contador_testes_negativos+contador_testes_positivos) * 100))
+
+
+def level_return(lista, nivel):
+    contador_testes_negativos = 0
+    contador_testes_positivos = 0
+    contador_nivel = 0
+    for teste in lista:
+        if teste.level == nivel:
+            if teste.result == "ok":
+                contador_testes_positivos+=1
+            else:
+                contador_testes_negativos+=1
+            contador_nivel+=1
+    print("\t\t-----------")
+    print(f"\t\t- Nivel {nivel} -")
+    print("\t\t-----------")
+    print(f"\t. O Numero de Testes Executados: {contador_testes_negativos + contador_testes_positivos}")
+    print(f"\t. O Numero de Testes Positivos: {contador_testes_positivos}")
+    print(f"\t. O Numero de Testes Negativos: {contador_testes_negativos}")
+    print(f"\t. A Percentagem de Falhas: %0.2f %% \n" % (
+                contador_testes_negativos / (contador_testes_negativos + contador_testes_positivos) * 100))
+
+i = 1
+max_level = 0
+for teste in lista:
+    if teste.level > max_level:
+        max_level = teste.level
+while i < max_level+1:
+    level_return(lista, i)
+    i+=1
+
+#for teste in lista:
+#    print(f"{teste.result}\t{teste.stage}\t{teste.description}\t{teste.level}")
 
 writeFile(html_file, "\n</body>\n</html>")
 
--- testes/teste3.t
+++ testes/teste3.t
@@ -1,11 +1,11 @@
 1..3
 ok 1
-ok 2
+not ok 2
 # Subtest: Some subtests first
-    ok 1 - my subtest 1
+    not ok 1 - my subtest 1
     # Subtest: Some subtests second
         ok 1 - my other subtest 1
-        ok 2 - and another subtest 2
+        not ok 2 - and another subtest 2
         1..2
     ok 2 - Some subtests second
     ok 3 - some more subtests
